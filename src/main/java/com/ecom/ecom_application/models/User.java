a9b6cbb73561cda79921102f3b8e0ce552f4f6dd
AungMinThant <amthantwork@gmail.com>
2026-06-19 10:17:32 +0630

    Entity Relationships: User & Address Mapping

--- src/main/java/com/ecom/ecom_application/models/User.java
+++ src/main/java/com/ecom/ecom_application/models/User.java
@@ -1,9 +1,6 @@
 package com.ecom.ecom_application.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -21,4 +18,8 @@ public class User {
     private String email;
     private String phone;
     private UserRole userRole = UserRole.CUSTOMER;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
 }
