17058044293c8071f8e703137ebed2c98488097c
AungMinThant <amthantwork@gmail.com>
2026-07-01 10:11:12 +0630

    Order Entity & Repository Design

---
+++ src/main/java/com/ecom/ecom_application/models/OrderItem.java
@@ -0,0 +1,26 @@
+package com.ecom.ecom_application.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity(name = "order_item")
+@Data
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="product_id", nullable = false)
+    private Product product;
+    private Integer quantity;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name="order_id", nullable = false)
+    private Orders order;
+}
---
+++ src/main/java/com/ecom/ecom_application/models/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.ecom.ecom_application.models;
+
+public enum OrderStatus {
+    PENDING, CONFIRMED, SHIPPED, DELIVERED, CANCELLED
+}
